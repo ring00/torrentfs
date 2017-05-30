01cd50f949aa9a6c6a26dd8123aaf37e1ac29c22
ZhangWei <suvtab@gmail.com>
2017-05-30 23:29:32 +0800

    ready for test

--- torrentfs.go
+++ torrentfs.go
@@ -1,157 +1,208 @@
 package torrentfs
 
 import (
-    "os"
-    "fmt"
-    "path/filepath"
-    "strings"
-    fusefs "bazil.org/fuse/fs"
-    "github.com/anacrolix/torrent"
-    "github.com/anacrolix/torrent/metainfo"
+	"bazil.org/fuse"
+	"bazil.org/fuse/fs"
+	"github.com/anacrolix/torrent"
+	"github.com/anacrolix/torrent/metainfo"
+	"golang.org/x/net/context"
+	"os"
+)
+
+const (
+	readOnly = 0555
 )
 
 type TorrentFS struct {
-    client *torrent.Client
+	root *RootNode
 }
 
-// Root returns the Node for the file system root.
-func (fs TorrentFS) Root() (Node, error) {
-    return Root{&fs}, nil
+func (fs *TorrentFS) Root() (Node, error) {
+	return *root, nil
 }
 
-// Destroy cleans up the file system when it's shutting down.
-func (fs TorrentFS) Destory() {
-
+func (fs *TorrentFS) Destory() {
+	for node := fs.root.Child; node != nil; node = node.Sibling {
+		node.Torrent.Drop()
+	}
 }
 
-// Root implements both Node and Handle for the root directory.
-type Root struct {
-    fs *TorrentFS
+func New(client *torrent.Client) *TorrentFS {
+	root := &RootNode{client, nil}
+	for _, torrent := range client.Torrents() {
+		node := newNode(torrent)
+		node.Sibling = root.Child
+		root.Child = node
+	}
+	return &TorrentFS{root}
+}
+
+type RootNode struct {
+	Client *torrent.Client
+	Child  *Node
+}
+
+func (root *RootNode) Update() {
+	for _, torrent := range root.Client.Torrents() {
+		update := true
+		for node := root.Child; node != nil; node = node.Sibling {
+			if node.Name == torrent.Name() {
+				update = false
+				break
+			}
+		}
+
+		if update {
+			added := newNode(torrent)
+			added.Sibling = root.Child
+			root.Child = added
+		}
+	}
+}
+
+func (root *RootNode) Attr(ctx context.Context, attr *fuse.Attr) error {
+	attr.Mode = os.ModeDir
+	return nil
+}
+
+func (root *RootNode) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	for node := root.Child; node != nil; node = node.Sibling {
+		if node.Name == name {
+			return *node, nil
+		}
+	}
+	return Node{}, fuse.ENOENT
+}
+
+func (root *RootNode) ReadDirAll(ctx context.Context) (entry []fuse.Dirent, err error) {
+	root.Update()
+	for node := root.Child; node != nil; node = node.Sibling {
+		entry = append(entry, fuse.Dirent{Type: node.Type, Name: node.Name})
+	}
+	return entry, err
 }
 
 type Node struct {
-    path []string
-    info *metainfo.Info
-    fs *TorrentFS
-    torrent *torrent.Torrent
+	// left-child right-sibling binary tree
+	Child, Sibling *Node
+	// The torrent of this node
+	Torrent *torrent.Torrent
+	// Type of this node, may be fuse.DT_File or fuse.DT_Dir
+	Type fuse.DirentType
+	// Name of this node
+	Name string
+	// Size of this node
+	Length int64
+	// Offset of the corresponding content in .torrent
+	Offset int64
 }
 
-type Dir struct {
-    Node
+func getTypeFromPath(path []string) fuse.DirentType {
+	if len(path) == 1 {
+		return fuse.DT_File
+	} else {
+		return fuse.DT_Dir
+	}
 }
 
-type File struct {
-    Node
-    size int64
-    offset int64
+func (node *Node) Insert(path []string, length int64, offset int64) {
+	var root *Node = nil
+
+	for child := node.Child; child != nil; child = child.Sibling {
+		if child.Name == path[0] {
+			root = child
+			break
+		}
+	}
+
+	if root == nil {
+		root = &Node{
+			Sibling: node.Child,
+			Torrent: node.Torrent,
+			Type:    getTypeFromPath(path),
+			Name:    path[0],
+			Length:  length,
+			Offset:  offset,
+		}
+		node.Child = root
+	} else {
+		root.Length += length
+	}
+
+	if len(path) > 1 {
+		root.Insert(path[1:], length, offset)
+	}
 }
 
-// Attr fills attr with the standard metadata for the node.
-func (root Root) Attr(ctx context.Context, attr *fuse.Attr) error {
-    attr.Mode = os.ModeDir
-    return nil
+func (node *Node) Build(info *metainfo.Info) {
+	for _, finfo := range info.Files {
+		node.Insert(finfo.Path, finfo.Length, finfo.Offset(info))
+	}
 }
 
-// Lookup looks up a specific entry in the receiver,
-// which must be a directory.  Lookup should return a Node
-// corresponding to the entry.  If the name does not exist in
-// the directory, Lookup should return ENOENT.
-func (root Root) Lookup(ctx context.Context, name string) (node fusefs.Node, err error) {
-    for _, torrent := range root.fs.client.Torrents() {
-        info = torrent.Info()
-        if info == nil || torrent.Name() != name {
-            continue
-        }
-
-        _node := Node{make([]string), info, root.fs, torrent}
-
-        if info.IsDir() {
-            node = Dir{_node}
-        } else {
-            node = File{_node, int64(info.Length), 0}
-        }
-        break
-    }
-    if node == nil {
-        err = fuse.ENOENT
-    }
-    return
+func (node *Node) Attr(ctx context.Context, attr *fuse.Attr) error {
+	attr.Size = node.Length
+	attr.Mode = readOnly
+	if node.Type == fuse.DT_Dir {
+		attr.Mode |= os.ModeDir
+	}
+	return nil
 }
 
-// ReadDirAll returns all Dirent under the root directory.
-func (root Root) ReadDirAll(ctx context.Context) (dirents []fuse.Dirent, err error) {
-    for _, torrent := range root.fs.client.Torrents() {
-        info = torrent.Info()
-        if info == nil {
-            continue
-        }
-
-        var _type fuse.DirentType
-        if info.IsDir() {
-            _type = fuse.DT_Dir
-        } else {
-            _type = fuse.DT_File
-        }
-
-        dirents = append(dirents, fuse.Dirent{info.Name, _type})
-    }
-    return
+func (node *Node) Lookup(ctx context.Context, name string) (fs.Node, error) {
+	for child := root.Child; child != nil; child = child.Sibling {
+		if child.Name == name {
+			return *child, nil
+		}
+	}
+	return Node{}, fuse.ENOENT
 }
 
-func (root Root) Forget() {
-    rn.fs.Destroy()
+func (node *Node) ReadDirAll(ctx context.Context) (entry []fuse.Dirent, err error) {
+	for child := root.Child; child != nil; child = child.Sibling {
+		entry = append(entry, fuse.Dirent{Type: child.Type, Name: child.Name})
+	}
+	return entry, err
 }
 
-func (dir Dir) Attr(ctx context.Context, attr *fuse.Attr) error {
-    attr.Mode = os.ModeDir | 0555
-    return nil
+func (node *Node) Read(ctx context.Context, req *fuse.ReadRequest, resp *fuse.ReadResponse) error {
+	if node.Type == fuse.DT_Dir {
+		panic("Reading from a directory!")
+	}
+
+	var err error
+	done := make(chan struct{})
+
+	go func() {
+		defer close(done)
+
+		reader := node.Torrent.NewReader()
+		defer reader.Close()
+
+		_, err := reader.Seek(node.Offset+req.Offset, os.SEEK_SET)
+		if err != nil {
+			return
+		}
+
+		_, err = reader.Read(resp.Data[:req.Size])
+	}()
+
+	select {
+	case <-done:
+		return err
+	case <-ctx.Done():
+		return fuse.EINTR
+	}
 }
 
-func (dir Dir) Lookup(ctx context.Context, name string) (node fusefs.Node, err error) {
-
-}
-
-func isSubPath(path, basepath string) bool {
-    if len(basepath) == 0 {
-        return len(path) > 0
-    }
-
-    if len(path) <= len(basepath) {
-        return false
-    }
-
-    if !strings.HasPrefix(path, basepath) {
-        return false
-    }
-
-    return path[len(basepath)] == '/'
-}
-
-func (dir Dir) ReadDirAll(ctx context.Context) (dirents []fuse.Dirent, err error) {
-    visited := make(map[string]bool)
-    for _, file := range dir.info.Files {
-        if !isSubPath(strings.Join(file.Path, "/"), strings.Join(dir.path, "/")) {
-            continue
-        }
-        name = file.Path[len(dir.path)]
-        if visited[name] {
-            continue
-        }
-
-        visited[name] = true
-        var entryType fuse.DirentType;
-        if info.IsDir() {
-            entryType = fuse.DT_Dir
-        } else {
-            entryType = fuse.DT_File
-        }
-    }
-}
-
-func (file File) Attr(ctx context.Context, attr *fuse.Attr) error {
-
-}
-
-func (file File) Lookup(ctx context.Context, name string) (node fusefs.Node, err error) {
-
+func newNode(torrent *torrent.Torrent) *Node {
+	info := torrent.Info()
+	node := &Node{
+		Torrent: torrent,
+		Type:    fuse.DT_Dir,
+		Name:    torrent.Name(),
+		Length:  torrent.Length(),
+	}
+	node.Build(info)
+	return node
 }
